6834a63742dace30b86807791f4cd5b61fdac67c
AlnurZhanibek-inDrive <alnur.zhanibek@indriver.com>
2025-11-29 18:59:15 +0500

    fix: db conn

--- internal/database/database.go
+++ internal/database/database.go
@@ -9,12 +9,13 @@ import (
 
 func New() *sql.DB {
 	dbConfig := mysql.Config{
-		User:      os.Getenv("DB_USER"),
-		Passwd:    os.Getenv("DB_PASS"),
-		Net:       "tcp",
-		Addr:      os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT"),
-		DBName:    os.Getenv("DB_NAME"),
-		ParseTime: true,
+		User:                 os.Getenv("DB_USER"),
+		Passwd:               os.Getenv("DB_PASS"),
+		Net:                  "tcp",
+		Addr:                 os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT"),
+		DBName:               os.Getenv("DB_NAME"),
+		ParseTime:            true,
+		AllowNativePasswords: true,
 	}
 
 	db, err := sql.Open("mysql", dbConfig.FormatDSN())
